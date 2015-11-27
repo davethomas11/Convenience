c9620269a88158fc743ed86b82603839623ff788
davethomas11 <dave@assemblyco.com>
2015-11-26 16:41:27 -0800

    Initial Commit

---
+++ Convenience/Convenience.h
@@ -0,0 +1,19 @@
+//
+//  Convenience.h
+//  Convenience
+//
+//  Created by David Thomas on 2015-11-26.
+//  Copyright © 2015 David Thomas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Convenience.
+FOUNDATION_EXPORT double ConvenienceVersionNumber;
+
+//! Project version string for Convenience.
+FOUNDATION_EXPORT const unsigned char ConvenienceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Convenience/PublicHeader.h>
+
+
